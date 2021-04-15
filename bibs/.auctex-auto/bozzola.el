c1a0b20b20d15d786ce65e61be90e1b1c5d23cba
Gabriele Bozzola <sbozzolator@gmail.com>
2021-04-15 07:50:04 -0700

    rebuilding site Thu 15 Apr 2021 07:50:04 AM MST

---
+++ bibs/.auctex-auto/bozzola.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook
+ "bozzola"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "Bozzola:2017qbu"
+    "Bozzola:2019aaw"
+    "Bozzola:2019tit"
+    "Bozzola:2020mjx"
+    "Bozzola:2021hus"
+    "Bozzola:2021elc"))
+ :bibtex)
+
---
+++ bibs/.auctex-auto/bozzola2021elc.el
@@ -0,0 +1,7 @@
+(TeX-add-style-hook
+ "bozzola2021elc"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "Bozzola:2021elc"))
+ :bibtex)
+
